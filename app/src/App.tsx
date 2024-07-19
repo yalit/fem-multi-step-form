3cae0c91a3a2a3b36b77ea6d25b4589fa332fccd
Yannick <tech@yalit.be>
2024-07-19 11:40:46 +0200

    build : move to Vite + React + Typescript + Tailwind

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import './App.css';
 import { Sidebar } from './components/sidebar';
 
 function App() {
   return (
+        <>
     <div id="app">
         <Sidebar />
     </div>
+    </>
   );
 }
 
